1f92c43fb133d2f0f898c623112b1949bf169114
Drew <drew.ciaramitaro51@gmail.com>
2020-06-05 14:06:53 -0400

    Start to port the settings from old src

--- toontown/toonbase/ToontownStart.py
+++ toontown/toonbase/ToontownStart.py
@@ -22,6 +22,53 @@ except:
     from toontown.launcher.ToontownDummyLauncher import ToontownDummyLauncher
     launcher = ToontownDummyLauncher()
     builtins.launcher = launcher
+    
+from libotp.settings.JSONSettings import JSONSettings
+builtins.settings = JSONSettings()
+if 'fullscreen' not in settings:
+    settings['fullscreen'] = False
+if 'music' not in settings:
+    settings['music'] = True
+if 'sfx' not in settings:
+    settings['sfx'] = True
+if 'musicVol' not in settings:
+    settings['musicVol'] = 1.0
+if 'sfxVol' not in settings:
+    settings['sfxVol'] = 1.0
+if 'loadDisplay' not in settings:
+    settings['loadDisplay'] = 'pandagl'
+if 'toonChatSounds' not in settings:
+    settings['toonChatSounds'] = True
+if 'aspectRatio' not in settings:
+    settings['aspectRatio'] = 0
+if 'textureQuality' not in settings:
+    settings['textureQuality'] = 3
+if 'speakerMode' not in settings:
+    settings['speakerMode'] = 0
+if 'detailDistance' not in settings:
+    settings['detailDistance'] = 2
+if 'displayMode' not in settings:
+    settings['displayMode'] = 0
+if 'res' not in settings:
+    settings['res'] = [1280, 720]
+if 'camSensitivityX' not in settings:
+    settings['camSensitivityX'] = 0.31
+if 'camSensitivityY' not in settings:
+    settings['camSensitivityY'] = 0.21
+if 'controllerBrand' not in settings:
+    settings['controllerBrand'] = 0
+loadPrcFileData('Settings: res', 'win-size 800 300')
+loadPrcFileData('Settings: fullscreen', 'fullscreen 0')
+loadPrcFileData('Settings: undecorated', 'undecorated 0')
+loadPrcFileData('Settings: music', 'audio-music-active %s' % settings['music'])
+loadPrcFileData('Settings: sfx', 'audio-sfx-active %s' % settings['sfx'])
+loadPrcFileData('Settings: musicVol', 'audio-master-music-volume %s' % settings['musicVol'])
+loadPrcFileData('Settings: sfxVol', 'audio-master-sfx-volume %s' % settings['sfxVol'])
+loadPrcFileData('Settings: loadDisplay', 'load-display %s' % settings['loadDisplay'])
+loadPrcFileData('Settings: toonChatSounds', 'toon-chat-sounds %s' % settings['toonChatSounds'])
+loadPrcFileData('Settings: Speaker Mode',
+                'fmod-speaker-mode %s' % ["stereo", "quad", "surround", "5.1", "7.1"][settings['speakerMode']])
+loadPrcFileData('', 'ffmpeg-prefer-libvpx 1')
 
 launcher.setRegistry('EXIT_PAGE', 'normal')
 pollingDelay = 0.5
